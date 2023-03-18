07e1d9a28fe7ace6116006a58bed3771b63b17c1
mlwebmaster <mlyatavskiy@gmail.com>
2023-03-18 21:57:50 +0200

    v-0.17
    version-0.17

--- dist/js/app.js
+++ dist/js/app.js
@@ -5255,7 +5255,9 @@
         const da = new DynamicAdapt("max");
         da.init();
         const rangeSlider = document.getElementById("range-slider"), input0 = document.getElementById("input0"), input1 = document.getElementById("input1");
+        const rangeSlider2 = document.getElementById("range-slider2"), input3 = document.getElementById("input3"), input4 = document.getElementById("input4");
         const inputs = [ input0, input1 ];
+        const inputs2 = [ input3, input4 ];
         if (rangeSlider) {
             noUiSlider.create(rangeSlider, {
                 start: [ 500, 1e4 ],
@@ -5275,6 +5277,30 @@
                 rangeSlider.noUiSlider.set(arr);
             };
             inputs.forEach(((el, index) => {
+                el.addEventListener("change", (e => {
+                    setRangeSlider(index, e.currentTarget.value);
+                }));
+            }));
+        }
+        if (rangeSlider2) {
+            noUiSlider.create(rangeSlider2, {
+                start: [ 500, 1e4 ],
+                connect: true,
+                step: 1,
+                range: {
+                    min: [ 500 ],
+                    max: [ 1e4 ]
+                }
+            });
+            rangeSlider2.noUiSlider.on("update", (function(values, handle) {
+                inputs2[handle].value = Math.round(values[handle]);
+            }));
+            const setRangeSlider = (i, value) => {
+                let arr = [ null, null ];
+                arr[i] = value;
+                rangeSlider.noUiSlider.set(arr);
+            };
+            inputs2.forEach(((el, index) => {
                 el.addEventListener("change", (e => {
                     setRangeSlider(index, e.currentTarget.value);
                 }));
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -8,7 +8,12 @@ const rangeSlider = document.getElementById('range-slider'),
 input0 = document.getElementById('input0'),
 input1 = document.getElementById('input1');
 
+const rangeSlider2 = document.getElementById('range-slider2'),
+input3 = document.getElementById('input3'),
+input4 = document.getElementById('input4');
+
 const inputs = [input0, input1];
+const inputs2 = [input3, input4];
 
 if (rangeSlider) {
    noUiSlider.create(rangeSlider, {
@@ -36,13 +41,33 @@ if (rangeSlider) {
       setRangeSlider(index, e.currentTarget.value);
    })
   });
+}
 
 
+if (rangeSlider2) {
+   noUiSlider.create(rangeSlider2, {
+      start: [500, 10000],
+      connect: true,
+      step: 1,
+      range: {
+          'min': [500],
+          'max': [10000]
+      }
+  });
 
+  rangeSlider2.noUiSlider.on('update', function(values, handle){
+   inputs2[handle].value = Math.round(values[handle]);
+  });
 
+  const setRangeSlider = (i, value) => {
+   let arr = [null, null];
+   arr[i] = value;
+   rangeSlider.noUiSlider.set(arr);
+  };
 
-
-
-
-
+  inputs2.forEach((el, index) => {
+   el.addEventListener('change', (e) => {
+      setRangeSlider(index, e.currentTarget.value);
+   })
+  });
 }
